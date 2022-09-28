b0e5a222db9830933039e1ddee5afb1a1a380a4d
Ro11ingButler <rollingkai1214@gmail.com>
2022-09-29 02:10:11 +0800

    Add navbar color change

--- components/Hero.js
+++ components/Hero.js
@@ -8,8 +8,9 @@ export default function Hero() {
     return (
         <div className="flex mx-auto xl:flex-row md:flex-col h-screen pt-20">
             <section className="basis-1/2 p-20 grid place-content-center">
-                <h1 className="text-4xl p-4 text-white font-bold">Culpa eiusmod elit ipsum commodo et ea quis nisi.</h1>
-                <p className="p-4 text-slate-400">Cupidatat irure laborum ullamco proident occaecat veniam deserunt aliqua duis ea consectetur voluptate aliquip. Enim fugiat nisi occaecat excepteur velit nisi cillum minim. Aute pariatur duis anim aliquip dolore pariatur aliquip id esse qui commodo.</p>
+                <h1 className="text-4xl p-4 text-white font-bold">We are breaking the Generative PFP meta</h1>
+                <p className="p-4 text-slate-400"><strong className="text-slate-200">Menacing Chibis</strong> is not just your typical PFP (Profile Picture) NFT. You&apos;re minting a character design; a brand; an identity. Menacing Chibis is a customizable, gamified NFT PFP collection from Rolling-kai. The goal is to &quot;mint your own Chibi&quot; from a pool of traits and upgrade them through mini-games in our discord server.</p>
+                <p className="p-4 text-slate-400">&ldquo;It seems difficult to draw a fully-rendered (realistic head and body) character. But if you have a Chibi, it&apos;s easier to draw. So I put the details behind and think of a Chibi first.&rdquo; -- Naoki Saito</p>
                 <div className="p-4 flex gap-8">
                     <Button outline>Mint Now</Button>
                     <Button solid>Contract Address</Button>
@@ -17,7 +18,7 @@ export default function Hero() {
             </section>
             <section className="basis-1/2 grid grid-flow-col place-content-center">
                 <Card className="translate-x-16 " image={hitrun}></Card>
-                <Card className="z-10 scale-125" image={rb}></Card>
+                <Card className="z-10 scale-[1.30]" image={rb}></Card>
                 <Card className="-translate-x-16 " image={jgt}></Card>
                 {/* <Card className="absolute mx-auto "></Card> */}
                 {/* <Card className="my-20 absolute"></Card> */}
--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from "react"
 import NavItem from "./NavItem"
 
-export default function Nav () {
+export default function Nav() {
+
+    const navRef = useRef()
+    
+    useEffect(() => {
+        function toggleNavBarColor() {
+            if(window.scrollY > navRef.current.clientHeight){
+                navRef.current.classList.add("bg-slate-900/95")
+            } else {
+                navRef.current.classList.remove("bg-slate-900/95")
+            }
+        }
+
+        window.addEventListener("scroll", toggleNavBarColor)
+ 
+        return () => {
+            window.removeEventListener("scroll", toggleNavBarColor)
+        }
+    })
+
     return (
         <nav>
-            <ul className="flex flex-row place-content-center fixed top-0 left-0 right-0 z-20 text-white font-semibold">
+            <ul className="flex flex-row place-content-center fixed top-0 left-0 right-0 z-20 text-white font-semibold bg-slate-900" ref={navRef}>
                 <NavItem href="/" logo>Logo</NavItem>
                 <NavItem href="/">Home</NavItem>
                 <NavItem href="/">Home</NavItem>
